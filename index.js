dc95ad9522e50adacbb0c9dd2d205326f3bd084e
VINOTH <Vinothkumar.Balaraman@aa.com>
2019-05-27 12:34:19 +0530

    added get api

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/guggy', (req, res) => {
+app.post('/guggyToSlack', (req, res) => {
 	
 	let sentence = req.body.text; 
 	
@@ -39,6 +39,28 @@ app.post('/guggy', (req, res) => {
 		
 		console.log('Response Code from guggy for sentence "' + sentence + '" is ' + response.statusCode);
 		res.send(jsonContent.animated[0].gif.hires.url);
+	});
+});
+
+app.get('/guggy', (req, res) => {
+	
+	var sentence = req.query.s; 
+	
+	request.post({
+		'headers': { 'content-type': 'application/json', 'apiKey' : process.env.apikey },
+		'url' : 'http://text2gif.guggy.com/v2/guggify', 
+		'body': JSON.stringify({
+			'sentence':  sentence,
+			'lang': 'ru'
+		})
+	},(error, response, body) => {
+		if(error) {
+			res.send(error)
+		}
+		var jsonContent = JSON.parse(body);
+		
+		console.log('Response Code from guggy for sentence "' + sentence + '" is ' + response.statusCode);
+		res.redirect(jsonContent.animated[0].gif.hires.url);
 	});
 });
 		
